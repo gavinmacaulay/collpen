47e61ee8ea8700df57c5ce98bea2742582798d87
gavinj <gavinj@localhost>
2012-06-28 21:39:00 +0200

    Add in TDMS conversion code

--- matlabtoolbox/Contents.m
+++ matlabtoolbox/Contents.m
@@ -2,9 +2,11 @@
 % 
 % File converion functions:
 % cp_ConvertExposurepar - Cov
+%
 % cp_ConvertCurrent - reads and converts the CARUSO power supply current measurement files into a Matlab formatted file. 
 %                        Will merge together multiple files into one Matlab file.
 %
+% cp_ConvertTDMStoMat - reads and convert the NI TDMS hydrophone files into Matlab format files.
 %
 % Data processing functions:
 % cp_CalibrationConstant - Get the calibration constant for each frequency for the tone stimuli
