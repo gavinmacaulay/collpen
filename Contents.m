65fb886c6a849c29dcb6c851ed038f6d3347d637
nilsolav <nilsolav@OBSE6803-4.imr.no>
2012-06-29 18:38:53 +0200

    Added a TODO file as a Contents.m file.

---
+++ Contents.m
@@ -0,0 +1,14 @@
+% 
+% TODO list for different functions - This needs revision!
+%
+% Read and process matlab files
+% Read and process Didson files
+% Read and process tdms files
+% Create spectrograms from tdms files
+% Read metadata files
+%
+% Block treatment from vessel noise
+% Block treatment from tones 
+%
+%
+%
