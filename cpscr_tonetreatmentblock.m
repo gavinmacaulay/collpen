07abfbe196463ad8c45e957fbd2a8141e3e06167
nilsolav <nilsolav@OBSE6803-4.imr.no>
2012-07-04 14:30:02 +0200

    Changed the tonetreatmentblock. This is the vesrion used for the block in the afternoon on July 4.

--- cpscr_tonetreatmentblock.m
+++ cpscr_tonetreatmentblock.m
@@ -1,18 +1,18 @@
 clear
-%  
+%
 % The purpose of this test is to show the relative importance of the
 % parameters frequency, rise, time and source level. Combintation from the
-% different variables are run in a randomized order. 
+% different variables are run in a randomized order.
 %
 % NB: Note that the gain needs to be adjusted on the amplifier during trials.
-%  
+%
 % Range of parameters to be tested:
-%  
+%
 % Frequency – 160, 320, 500 Hz
 % Source level – 157 167 177 dB
 % Tone duration – 2000 ms
 % Tone rise time 18, 31, 250 ms
-% 
+%
 % Two upsweep signals are added at random in between the tone treatments
 %
 % Based on previous experiments (e.g. Karlsen & Echof, 2011, LowFreq
@@ -20,29 +20,32 @@ clear
 % important parameters, while duration and source level will likely be of
 % less importance. However, this is a simple experiment to verify this
 
+%par.filePath='c:\Collpen\Processing\'  % path to write output files to
+par.filePath = './';  % path to write output files to
 % Frequency range
 par.f1_0 = [160 320 500];%Hz
+par.f_sweep = [160 500];% Frequency sweep
 
 % SL range
-par.RL_0 = [145 160 177];%dB
+par.RL_0 = [155 165 175];%dB
+par.RL_sweep = 175;%Received level
 
 % Duration
 par.dur_0 = 2000;%ms
+par.dur_sweep = 5000;%ms
 
 % Inswing range
 par.rt_0 = [18 31 250];%ms
 par.rt_end = 1800;%ms "outswing"
-
-% Frequency sweep treatment
-par.f_sweep = [160 500];% Frequency sweep
 par.rt_sweep = 250;%Rise time
-par.RL_sweep = 177;%Received level
-par.dur_sweep = 5000;%ms
- 
+
+
 par.Fs = 8192;% Hz "sampling frequency"
-par.N = 10;% Number of replicas in this block
 par.Dt = 30;% Time between treatments within block
+par.Dt = 1;% Time between treatments within block
 par.range = 1;%m Set this to one for SL's instead of RL's
+par.N = 10;% Number of replicas in this block - sweeps
+
 
 %% Create treatment data
 
@@ -82,7 +85,10 @@ par0.dur(k) = par.dur_0;
 K=k;
 
 % Randomize the order of the treatments
-Ni = randperm(k);
+sub = randperm(K-2);
+sub2 = [sub(1:par.N) K-1 K];
+Ni = sub2(randperm(par.N+2));
+
 par.F1 = par0.F1(Ni);
 par.F2 = par0.F2(Ni);
 par.RL = par0.RL(Ni);
@@ -98,22 +104,26 @@ N_sweep = par.dur_sweep/1000*par.Fs;
 t_tone = (1:N)/par.Fs;
 t_sweep = (1:N_sweep)/par.Fs;
 
-for i=1:K
+for i=1:length(par.F2)
     if par.F1(i)==par.F2(i)     % Tone signal
         % Raw singal
         t = t_tone;
         y_raw = sin(t*par.F1(i)*2*pi);
         % Add calibration
-        [par.k(i) ampGain carusoCurrent] = cp_CalibrationConstant(par.F1(i), par.RL(i), par.range);
-        p_raw = par.k(i).*y_raw;
+        [k par.ampGain(i) par.carusoCurrent(i)] = cp_CalibrationConstant(par.F1(i), par.RL(i), par.range);
+        p_raw = k.*y_raw;
     else % The sweep signal
         % Frequency vector
         t = t_sweep;
         Fsweep = (1:N_sweep)./N_sweep*(par.F2(i)-par.F1(i))+par.F1(i);
         % Raw sweep signal
         y_raw = sin(t.*Fsweep.*2.*pi);
-        %warning('TODO: Add frequency dependent calibration!!!')
-        [k ampGain carusoCurrent] = cp_CalibrationConstant(Fsweep, par.RL(i), par.range);
+        [k tempAmpGain tempCarusoCurrent] = cp_CalibrationConstant(Fsweep, par.RL(i), par.range);
+        % assigning Gain and Current to list of parameters
+        par.ampGain(i)=tempAmpGain(1);
+        par.carusoCurrent(i)=tempCarusoCurrent(1);
+        % adjsuting sweep for caruso response (k i s frequency dependent
+        % vector)
         p_raw = k.*y_raw;
     end
     
@@ -126,24 +136,60 @@ for i=1:K
     p = p_raw.*y_env;
     
     % Play back signal and display gain information on screen
+    
+    disp(['NEXTTONE Set AmpGain=',num2str(par.ampGain(i)),'dB! AmpCurrent=', num2str(par.carusoCurrent(i)),' F2=',num2str(par.F1(i)),' F2=',num2str(par.F2(i)),'Hz, SL=',num2str(par.RL(i)),'dB, rt=',num2str(par.rt(i)),'ms'])
+    
+    if i>1
+        disp(['Waiting for ' num2str(par.Dt) ' s before next sound.'])
+        pause(par.Dt)
+    end
+    disp('Press any key when amplifier gain is set.')
+    pause
+    disp(['TX: ',num2str(i), ' AmpGain=',num2str(par.ampGain(i)),'dB! AmpCurrent=', num2str(par.carusoCurrent(i)),' F1=',num2str(par.F1(i)),' F2=',num2str(par.F2(i)),'Hz, SL=',num2str(par.RL(i)),'dB, rt=',num2str(par.rt(i)),'ms'])
 
-    disp(['Set AmpGain=',num2str(ampGain),'dB!! F=',num2str(par.F1(i)),'Hz, SL=',num2str(par.RL(i)),'dB, rt=',num2str(par.rt(i)),'ms'])
-    %     for j=0:9
-    %         disp(num2str(10-j))
-    %         pause(1)
-    %     end
-    disp(['Waiting for ' num2str(par.Dt) ' s before next sound.'])
-    pause(par.Dt)
-    input('Press any key when amplifier gain is set.')
-    disp(' ')
-    %     close all
-    %     plot(t,p)
-    disp(['TX: AmpGain=',num2str(ampGain),'dB, F=',num2str(par.F1(i)),'Hz, SL=',num2str(par.RL(i)),'dB, rt=',num2str(par.rt(i)),'ms'])
     par.treat_start(i) = now;
     sound(p,par.Fs)
     par.treat_stop(i) = now;
-    disp(['TX ending, wait',num2str(par.Dt-10),'s'])
-
+    
+    disp('.') % leave some space for legibility
+    disp('.') % leave some space for legibility
+    
 end
 
-save(['HerringExp_toneTreatment_par_',datestr(now,30),'.mat'],'par')
+%set filename
+fname=strcat(par.filePath ,'ToneParams_',datestr(now,30));
+
+eval(['save ', fname])   % save parameter file in mat format
+
+% now build up the output for excel
+% prepare a cell array for export
+
+% headers
+a{1,1}='t_start_time';
+a{1,2}='t_stop_time';
+a{1,3}='t_soundsource';
+a{1,4}='t_carusocurrent';
+a{1,5}='t_amplifiergain';
+a{1,6}='t_F1';
+a{1,7}='t_F2';
+a{1,8}='t_SL';
+a{1,9}='t_duration';
+a{1,10}='t_rt';
+
+% place data in cell array
+for i=2:length(par.F1)+1
+    a{i,1}=datestr(par.treat_start(i-1),'dd.mm.yy HH:MM:SS');
+    a{i,2}=datestr(par.treat_stop(i-1),'dd.mm.yy HH:MM:SS');
+    a{i,3}='Caruso';
+    a{i,4}=par.carusoCurrent(i-1);
+    a{i,5}=par.ampGain(i-1);
+    a{i,6}=par.F1(i-1);
+    a{i,7}=par.F2(i-1);
+    a{i,8}=par.RL(i-1);
+    a{i,9}=par.dur(i-1);
+    a{i,10}=par.rt(i-1);
+end
+
+xlswrite(fname,a) % write out xls file
+
+disp('Finished !')
