30b673647da094ea3d5efec2ba584a18bdc1f7c1
ressler <ressler@mace-fieldlt-2.lysglimt.net>
2012-07-06 11:15:36 -0700

    test of bitbucket (tutorial with NOH)

--- cpscr_orcatreatmentblock.m
+++ cpscr_orcatreatmentblock.m
@@ -7,13 +7,14 @@
 clear
 par.wavName{1,1}= 'NorwegianOrcaCalls.wav';
 par.wavName{2,1}= 'CanadianOrcaCalls.wav';
-par.wavName{3,1}= 'IcelandicOrcaCalls.wav';
+par.wavName{3,1}= 'IcealandicOrcaCalls_Dtag_ch1.wav';
 
+par.ampGain=[-5 -5 -10]; % amp gain corresponding to [norwegian, canadian, icelandic] calls
 
 par.playBackDuration=[60 60 60]; % duration of playback in sec
-par.playBackStartPoint=[30 30 30]; % place in the file to start in seconds  (starting 30 sec in as boat noise at beginning of one file
-par.waitTime=120; % duration pause between playbacks  in s
-par.soundCard='70 %';
+par.playBackStartPoint=[30 30 10]; % place in the file to start in seconds  (starting 30 sec in as boat noise at beginning of one file
+par.waitTime=1; % duration pause between playbacks  in s
+par.soundCard='100 %';
 par.amplifier='Lubell';
 par.filePath='.\';  % path to write output files to
 par.forceSoundPause=0; %whether to force a pause duining playback (Alex PC=1, Nils Olav=0)
@@ -21,7 +22,7 @@ par.forceSoundPause=0; %whether to force a pause duining playback (Alex PC=1, Ni
 %%%%%%% prompt for changes
 disp('Check Lubell is connected and hit any key')
 pause
-disp('check that card is set to 70% and hit a key')
+disp('check that card is set to 100% and hit a key')
 pause
 disp('READY? the experiment starts on next keypress')
 pause
@@ -29,13 +30,18 @@ pause
 %%%%  present the stimuli in random order
 
 % seed the random number generator
-%reset(RandStream.getDefaultStream,sum(100*clock)) % works in r2010b and r2012a
-reset(RandStream.getGlobalStream,sum(100*clock))  % works in r2012a
-par.randTrial=randperm(3)
+
+reset(RandStream.getDefaultStream,sum(100*clock)) % works in r2010b and r2012a
+%reset(RandStream.getGlobalStream,sum(100*clock))  % works in r2012a
+par.randTrial=randperm(3);
 for i=1:length(par.randTrial)
     
+    disp(['Next Playback: ',par.wavName{par.randTrial(i),1}])
+    disp(['Set AmpGain to: ' num2str(par.ampGain(par.randTrial(i))) ' and press any key to start playback']) 
+    disp('.')
+    pause
     [y, Fs] = wavread(par.wavName{par.randTrial(i),1});
-    disp(['Playback: ',num2str(i),' ', par.wavName{par.randTrial(i),1}, ' Duration = ',num2str(par.playBackDuration) ])
+    disp(['Playback: ',num2str(i),' ', par.wavName{par.randTrial(i),1}, ' Duration = ',num2str(par.playBackDuration(i)) ])
     ind=(1:Fs*par.playBackDuration)+par.playBackStartPoint(1);
     disp(par.wavName{par.randTrial(i),1})
     par.treatStart(i)=now;
