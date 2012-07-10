b275d3211d1eb0952413de327f05504eacfcec23
Boswell <Boswell@Boswell-PC.lysglimt.net>
2012-07-10 17:50:41 -0400

    Added the log and changed the get exp parameters.

---
+++ cpscr_processtimestampsdata.m
@@ -0,0 +1,71 @@
+%% Process hydrophone data and create figure in the figure directory
+clear
+close all
+
+% Data directory
+par.datadir = 'F:\collpen\AustevollExp\data\HERRINGexp';
+par.datadir = 'C:\repositories\matlabtoolbox';
+
+% Parameters and metadata
+file = fullfile(par.datadir,'CollPenAustevollLog.xls');
+block = cp_GetExpPar(file);
+
+%% Block timeline
+clf
+
+subplot(121)
+k=1;
+ind =[1:6 11:12];
+for i=ind%length(block)
+    hold on
+    plot([block(i).b_starttime_mt block(i).b_starttime_mt],[0 1])
+    text(block(i).b_starttime_mt,(length(ind)-k)/length(ind),...
+        ['Block ',num2str(block(i).b_block),', ',block(i).b_starttime])
+    k=k+1;
+end
+datetick
+
+subplot(122)
+k=1;
+ind= [7:10 13:length(block)];
+for i=ind
+    hold on
+    plot([block(i).b_starttime_mt block(i).b_starttime_mt],[0 1])
+    text(block(i).b_starttime_mt,(length(ind)-k)/length(ind),...
+        ['Block ',num2str(block(i).b_block),', ',block(i).b_starttime])
+    k=k+1;
+end
+datetick   
+    
+%% Subblock timelines
+k=1;
+close all
+%ind= 1:length(block);
+ind= 17:length(block);
+
+for i=ind
+    figure
+    hold on
+    k=1;
+    N = length(block(i).subblock);
+    M = (12+5+3+3+4);
+    for j=1:N
+        disp(block(i).subblock(j))
+        plot([block(i).subblock(j).s_start_time_mt block(i).subblock(j).s_start_time_mt],[0 1])
+        plot([block(i).subblock(j).s_stop_time_mt block(i).subblock(j).s_stop_time_mt],[0 1])
+        text(block(i).subblock(j).s_start_time_mt,(M-k)/M,...
+            ['Block ',num2str(block(i).subblock(j).s_block),', Subblock ',...
+            num2str(block(i).subblock(j).s_subblock),', ',block(i).subblock(j).s_start_time],'interpreter','none')
+        k=k+1;
+        for l = 1:length(block(i).subblock(j).treatment)
+            plot([block(i).subblock(j).treatment(l).t_start_time_mt block(i).subblock(j).treatment(l).t_stop_time_mt],[(M-k)/M (M-k)/M])
+            plot(block(i).subblock(j).treatment(l).t_start_hydrophonePC_mt, (M-k)/M,'*')
+            k=k+1;
+        end
+    end
+    title(['Block ',num2str(block(i).b_block),', ',block(i).b_comments],'interpreter','none')
+    datetick
+    pause
+end
+
+
--- matlabtoolbox/cp_GetExpPar.m
+++ matlabtoolbox/cp_GetExpPar.m
@@ -34,10 +34,9 @@ cruise = cell2struct(raw(2:s(1),:),fields,2);
 
 % convert times and date
 % b_starttime 
-% for i=1:s(1)-1
-%     cruise(i).cruise_starttime = datenum(cruise(i).cruise_starttime,'dd.mm.yyyy HH:MM:SS');
-%     cruise(i).cruise_stoptime  = datenum(cruise(i).cruise_stoptime,'dd.mm.yyyy HH:MM:SS');
-% end
+ for i=1:s(1)-1
+     cruise(i).b_starttime_mt = datenum(cruise(i).b_starttime,'dd.mm.yyyy HH:MM:SS');
+ end
 
 function[cruise]=importsubblock(file)
 % import cruise structure
@@ -49,9 +48,23 @@ cruise = cell2struct(raw(2:s(1),:),fields,2);
 % s_start_time	s_stop_time
 % convert times and date
 % for i=1:s(1)-1
-%     cruise(i).cruise_starttime = datenum(cruise(i).cruise_starttime,'dd.mm.yyyy HH:MM:SS');
+%     cruise(i).b_starttime  = datenum(cruise(i).b_starttime,'dd.mm.yyyy HH:MM:SS');
 %     cruise(i).cruise_stoptime  = datenum(cruise(i).cruise_stoptime,'dd.mm.yyyy HH:MM:SS');
 % end
+for i=1:s(1)-1
+    try
+        cruise(i).s_start_time_mt = datenum(cruise(i).s_start_time,'dd.mm.yyyy HH:MM:SS');
+    catch
+        cruise(i).s_start_time_mt = NaN;
+        warning('s_stop_time did not convert')
+    end
+    try
+        cruise(i).s_stop_time_mt = datenum(cruise(i).s_stop_time,'dd.mm.yyyy HH:MM:SS');
+    catch
+        cruise(i).s_stop_time_mt = NaN;
+        warning('s_stop_time did not convert')
+    end
+end
 
 
 function[passing]=importtreatment(file)
@@ -61,6 +74,31 @@ s=size(raw);
 fields = txt(1,1:s(2));
 passing = cell2struct(raw(2:s(1),:),fields,2);
 
+for i=1:s(1)-1
+    try
+        passing(i).t_start_time_mt = datenum(passing(i).t_start_time,'dd.mm.yyyy HH:MM:SS');
+    catch
+        passing(i).t_start_time_mt = NaN;
+        warning('t_start_time did not convert')
+    end
+    try
+        passing(i).t_stop_time_mt = datenum(passing(i).t_stop_time,'dd.mm.yyyy HH:MM:SS');
+    catch
+        passing(i).t_stop_time_mt = NaN;
+        warning('t_stop_time did not convert')
+    end
+    try
+        passing(i).t_start_hydrophonePC_mt = datenum(passing(i).t_start_hydrophonePC,'dd.mm.yyyy HH:MM:SS');
+    catch
+        passing(i).t_start_hydrophonePC_mt = NaN;
+        warning('t_start_hydrophonePC did not convert')
+    end
+end
+
+%t_treatmenttype
+
+% 'orca_can','orca_is','orca_nor'
+% 'vessel_GOS','vessel_JH','vessel_GOSup'
 % t_start_time t_stop_time t_start_hydrophonePC 
 % convert times and date
 % for i=1:s(1)-1
