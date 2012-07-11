921be03a927a74a98abe7297d89f8ee0f7872517
Boswell <Boswell@Boswell-PC.lysglimt.net>
2012-07-11 06:18:43 -0400

    Added echosounder data proessing scripts. updated the parameter structure

--- cpscr_processechosounderdata.m
+++ cpscr_processechosounderdata.m
@@ -10,15 +10,9 @@ par.reposdir = 'C:\repositories\matlabtoolbox';
 file = fullfile(par.reposdir,'CollPenAustevollLog.xls');
 block = cp_GetExpPar(file);
 
-% Parameters for the spectrogram
-par.avgtime = 0.1;%s
-par.p_ref = 1e-6; % [Pa]
-par.ws=10000*60*4;%4 min of 8khz sampling
-par.Nwindow=256*4;
-par.Nfft=256*8;
-par.Noverlap=fix(par.Nwindow/2);
-
 %% Run
-for i=20
-        cp_ProcessHydrophonedata(block(i).b_block,block,par);
+for i=17:33
+        cp_ProcessEchosounderdata(block(i).b_block,block,par);
 end
+
+
