8a548dec760507f2e70f908bd38811443d25f83e
nilsolav <nilsolav@OBSE6803-4.imr.no>
2013-10-11 00:10:27 +0200

    First version of vessel avoidance work

--- vesselavoidancepaper/cpscr_vapaper_figures.m
+++ vesselavoidancepaper/cpscr_vapaper_figures.m
@@ -1,4 +1,6 @@
 %% This script plot the figures for the VA paper.
+clear
+close all
 
 par.datadir = '\\callisto\collpen\AustevollExp\data\HERRINGexp';
 par.reposdir = 'C:\repositories\CollPen_mercurial\matlabtoolbox';
@@ -7,40 +9,32 @@ file = fullfile(par.datadir,'CollPenAustevollLog.xls');
 block = cp_GetExpPar(file);
 
 % Pick 3 examples
-%F=160, SL=175, RT=18  : 22-2-12
-%F=320, SL=165, RT=31  : 23-1-3
-%F=500, SL=155, RT=250 : 27-4-11
+
+%21	1	1	block21_1_1
+%21	1	2	block21_1_2
+%21	1	3	block21_1_3
 
 % Figure 1(a)
-% F160RT18SL155 22_2_11
-F{1,1}=[22 2 11];
-% F160RT18SL165 28_2_9
-F{1,2}=[28 2 9];
-% F160RT18SL175 22_2_12
-F{1,3}=[22 2 12];
+F{1,1}=[21 1 1];
+F{1,2}=[21 1 2];
+F{1,3}=[21 1 3];
 
 % Figure 1(b)
-% F160RT18SL175
-F{2,1}=[22 2 12];
-% F160RT31SL175 23_1_11
-F{2,2}=[28 2 7];
-% F160RT250SL175 32_4_4
-F{2,3}=[32 4 4];
+F{2,1}=[21 1 1];
+F{2,2}=[21 1 2];
+F{2,3}=[21 1 3];
 
 % Figure 1(c)
-% F160RT18SL175
-F{3,1}=[22 2 12];
-% F320RT18SL175
-F{3,2}=[23 1 10];
-% F500RT18SL175
-F{3,3}=[24 2 10];
+F{3,1}=[21 1 1];
+F{3,2}=[21 1 2];
+F{3,3}=[21 1 3];
 
-%%
 par.avgtime = 0.1;%s
 par.p_ref = 1e-6; % [Pa]
 
 for i=1:3
     for j=1:3
+        disp(['i:',num2str(i),'/3 j:',num2str(j),'/3'])
         % Load hydrophone data
         file = fullfile(par.datadir,['block',num2str(F{i,j}(1))],'hydrophones',...
             [num2str(F{i,j}(1)),'_',num2str(F{i,j}(2)),'_',num2str(F{i,j}(3)),'.mat']);
@@ -70,20 +64,19 @@ for i=1:3
                 nexus.ch2.press_rms_avg(k)= (mean(temp.^2))^.5;
                 nexus.time(k) = ((mean([ind_start(k) ind_end(k)])))./(par.Fs);%./(par.Fs*24*60*60)) + par.start_time;
             end
-         
-              
+            
             t0 = par.start_time;
             t1 = block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_start_time_mt;
             dt = (t0-t1)*24*60*60;%s
-            disp(['F'  ,num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_F1),...
-                  '_SL',num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_SL),...
-                  '_RT',num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_rt)]);
+            %             disp(['F'  ,num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_F1),...
+            %                   '_SL',num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_SL),...
+            %                   '_RT',num2str(block(F{i,j}(1)).subblock(F{i,j}(2)).treatment(F{i,j}(3)).t_rt)]);
             
             % Export data to R
             switch i
                 case 1
                     % SPL
-                    DAT{i,j}.x=nexus.time+dt; 
+                    DAT{i,j}.x=nexus.time+dt;
                     DAT{i,j}.nexus1 = nexus.ch1.press_rms_avg./par.p_ref;
                     DAT{i,j}.nexus2 = nexus.ch2.press_rms_avg./par.p_ref;
                 case 2
@@ -92,10 +85,16 @@ for i=1:3
                     DAT{i,j}.nexus1 = nexus.ch1.press;
                     DAT{i,j}.nexus2 = nexus.ch2.press;
                 case 3
-                    % Welch Frequency Spectrum
+                    % Pick data around max pressure i.e. "passing" of the
+                    % "vessel"
+                    %t = par.start_time + (1:length(nexus.ch1.press))/(par.Fs*24*60*60);
+                    t = (1:length(nexus.ch1.press))/par.Fs;
+                    ind = t>18 & t<25;
+                    
+                    % Welch Frequency Spectrum (needs to calculated around max pressure!)
                     h = spectrum.periodogram;
-                    Hpsd1 = psd(h,double(nexus.ch1.press).*1e6,'Fs',par.Fs);
-                    Hpsd2 = psd(h,double(nexus.ch2.press).*1e6,'Fs',par.Fs);
+                    Hpsd1 = psd(h,double(nexus.ch1.press(ind)).*1e6,'Fs',par.Fs);
+                    Hpsd2 = psd(h,double(nexus.ch2.press(ind)).*1e6,'Fs',par.Fs);
                     DAT{i,j}.x      = Hpsd1.Frequencies;
                     DAT{i,j}.nexus1 = Hpsd1.Data;
                     DAT{i,j}.nexus2 = Hpsd2.Data;
@@ -106,21 +105,22 @@ for i=1:3
         end
     end
 end
-save DAT DAT
+save DATva DAT
+
 %% Export files to R/plot figures
 clear
-load DAT
+load DATva
 close all
 figure(1)
 clf
 subplot(131)
 % Figure 1(a)
 for i=1:3
-    ind{i}= DAT{1,i}.x>0 & DAT{1,i}.x<4;
+    ind{i}= DAT{1,i}.x>-inf & DAT{1,i}.x<inf;
 end
 plot(DAT{1,1}.x(ind{1}),20*log10(DAT{1,1}.nexus1(ind{1})),...
-     DAT{1,2}.x(ind{2}),20*log10(DAT{1,2}.nexus1(ind{2})),...
-     DAT{1,3}.x(ind{3}),20*log10(DAT{1,3}.nexus1(ind{3})))
+    DAT{1,2}.x(ind{2}),20*log10(DAT{1,2}.nexus1(ind{2})),...
+    DAT{1,3}.x(ind{3}),20*log10(DAT{1,3}.nexus1(ind{3})))
 ylabel('SPL (dB re 1\mu Pa)')
 xlabel('time (s)')
 
@@ -132,20 +132,20 @@ fig13=[DAT{1,3}.x(ind{3}); 20*log10(DAT{1,3}.nexus1(ind{3}))];
 % Figure 1(b)
 subplot(132)
 
-DAT{2,1}.x = DAT{2,1}.x - .8077;
-DAT{2,2}.x = DAT{2,2}.x - 1.4892;
-DAT{2,3}.x = DAT{2,3}.x - 1.3425;
+% DAT{2,1}.x = DAT{2,1}.x - .8077;
+% DAT{2,2}.x = DAT{2,2}.x - 1.4892;
+% DAT{2,3}.x = DAT{2,3}.x - 1.3425;
 
 for i=1:3
-    ind{i}= DAT{2,i}.x>-.1 & DAT{2,i}.x<.4;
+    ind{i}= DAT{2,i}.x>-inf & DAT{2,i}.x<inf;
 end
 % for i=1:3
 %     ind{i}= DAT{2,i}.x>-inf & DAT{2,i}.x<inf;
 % end
 
 plot(DAT{2,1}.x(ind{1})*1000,(DAT{2,1}.nexus2(ind{1})),'k',...
-     DAT{2,2}.x(ind{2})*1000,(DAT{2,2}.nexus2(ind{2})),'b',...
-     DAT{2,3}.x(ind{3})*1000,(DAT{2,3}.nexus2(ind{3})),'r')
+    DAT{2,2}.x(ind{2})*1000,(DAT{2,2}.nexus2(ind{2})),'b',...
+    DAT{2,3}.x(ind{3})*1000,(DAT{2,3}.nexus2(ind{3})),'r')
 ylabel('Pressure (Pa)')
 xlabel('time (ms)')
 
@@ -153,21 +153,96 @@ fig21=[DAT{2,1}.x(ind{1})*1000; DAT{2,1}.nexus2(ind{1})'];
 fig22=[DAT{2,2}.x(ind{2})*1000; DAT{2,2}.nexus2(ind{2})'];
 fig23=[DAT{2,3}.x(ind{3})*1000; DAT{2,3}.nexus2(ind{3})'];
 
-
 % Figure 1(c)
 subplot(133)
 plot(DAT{3,1}.x,10*log10(DAT{3,1}.nexus2),...
-     DAT{3,2}.x,10*log10(DAT{3,2}.nexus2),...
-     DAT{3,3}.x,10*log10(DAT{3,3}.nexus2))
+    DAT{3,2}.x,10*log10(DAT{3,2}.nexus2),...
+    DAT{3,3}.x,10*log10(DAT{3,3}.nexus2))
 xlim([100 600])
 xlabel('Frequency (Hz)')
 ylabel('Power spectral density (dB re 1\mu Pa Hz^{-1})')
 
-ind= DAT{3,1}.x<600 & DAT{3,1}.x>100;
+ind= DAT{3,1}.x<1000 & DAT{3,1}.x>10;
 fig31=[DAT{3,1}.x(ind)'; 10*log10(DAT{3,1}.nexus2(ind))'];
 fig32=[DAT{3,2}.x(ind)'; 10*log10(DAT{3,2}.nexus2(ind))'];
 fig33=[DAT{3,3}.x(ind)'; 10*log10(DAT{3,3}.nexus2(ind))'];
 
 save Figure1 fig11 fig12 fig13 fig21 fig22 fig23 fig31 fig32 fig33
 
+%% Prepare VA data
+
+clear
+par.datadir = '\\callisto\collpen\AustevollExp\data\HERRINGexp';
+par.reposdir = 'C:\repositories\CollPen_mercurial\matlabtoolbox';
+
+file = fullfile(par.datadir,'CollPenAustevollLog.xls');
+block = cp_GetExpPar(file);
+
+load(fullfile('C:\repositories\CollPen_mercurial\','VA.mat'));
+
+% Fit block informaion and va data
+N=1;
+%%
+
+% Les inn manuell scoring
+%b_block;b_groupsize;s_subblock;s_treatmenttype;t_treatment;t_treatmenttype;t_start_time_mt;t_stop_time_mt;t_F1;t_F2;t_SL;t_duration;t_rt;v_score
+%17;large group in M09;1;tones;1;tones_F1500_F2160_SL175_dur2000_rt250;735055.384456;735055.384525;500;160;175;2000;250;1.7
+
+str='%u;%*s;%u;%*s;%u;%*s;%*f;%*f;%*u;%*u;%*u;%*u;%*u;%f\n'
+
+
+fid=fopen('C:/repositories/CollPen_mercurial/score_anova_simple.csv','rt')
+getl(fid)
+scr = freadf(fid,str);
+fid=fclose(fid)
+
+VAvessel={'Block','Sub block','Treatment','sv_0','sv','m_0','m','Score','Type','Group size'};
+% Desse manglar:
+%27 3 2
+%27 3 3
+%33 4 1
+
+for b=21:35
+    for sb=1:length(block(b).subblock)
+        if strcmp(block(b).subblock(sb).s_treatmenttype,'vessel')
+            for trn=1:length(block(b).subblock(sb).treatment)
+                % Match with va data
+                switch block(b).subblock(sb).treatment(trn).t_treatmenttype
+                    case 'GOS_upscaled'
+                        tr = 'GOSup';
+                    case 'GOS_unfiltered'
+                        tr = 'GOS';
+                    case 'JH_unfiltered'
+                        tr = 'JH';
+                end
+                gs='NaN';
+                switch block(b).b_groupsize
+                    case 'large group in M09'
+                        gs='L';
+                    case 'small group in M09'
+                        gs='S';
+                end
+                ind=VA(:,1)==b & VA(:,2)==sb & VA(:,3)==trn;
+                indsc=scr(:,1)==b & scr(:,3)==sb & scr(:,5)==trn;
+                if sum(ind)>0
+                    vas = num2cell(VA(ind,:));% groupsize tr
+                else
+                    vas = num2cell([b sb trn NaN NaN NaN NaN]);% groupsize tr
+                end
+                
+                if sum(indsc)>0
+                    scrs = num2cell(scr(indsc,14));% groupsize tr
+                else
+                    scrs = num2cell(NaN);% groupsize tr
+                end
+                
+                VAvessel=[VAvessel;[vas scrs tr gs]];
+            end
+        end
+    end
+end
+disp(VAvessel)
+xlswrite('VAvessel.xls',VAvessel)
+
+save VAvessel VAvessel
 
--- vesselavoidancepaper/cpscr_vapaper_modelandfigures.r
+++ vesselavoidancepaper/cpscr_vapaper_modelandfigures.r
@@ -1,90 +1,8 @@
 setwd('/repositories/CollPen_mercurial/vesselavoidancepaper')
-
 library(R.matlab)
 
-file <- 'C:/repositories/CollPen_mercurial/score_anova_simple.csv'
-dat<-read.table(file,sep = ";",header = TRUE)
-names(dat)
-
-# Fix data
-
-#ind<-(dat$b_groupsize=="large group in M09")
-#dat$b_groupsize[ind]="large group"
-#dat$b_groupsize[!ind]="small group"
-
-
-# Checking data
-table(dat$b_block)
-table(dat$b_groupsize)
-table(dat$s_subblock)
-table(dat$s_treatmenttype)
-table(dat$t_treatment)
-table(dat$t_treatmenttype)
-table(dat$t_F2)
-table(dat$t_F2)
-table(dat$t_SL)
-table(dat$t_duration)
-table(dat$t_rt)
-table(dat$v_score)
-table(dat$t_treatmenttype)
-
-# Overview ANOVA
-dat_aov <- aov(dat$v_score ~ dat$t_treatmenttype)
-summary(dat_aov)
-
-# Is there a time effect?
-time<-(dat$t_start_time_mt - min(dat$t_start_time_mt))*60*24 # In minutes
-score <- dat$score
-plot(dat$v_score,time)
-tim <- lm(dat$v_score ~ time)
-summary(tim)
-
 #
-# Vessel noise
-#
-
-T2<-dat[(dat$s_treatmenttype=='vessel'),]
-
-# What is the mean and std score
-mean(T2$v_score)
-sd(T2$v_score)
-
-T2_aov <- aov(T2$v_score ~ T2$t_treatmenttype + T2$b_groupsize)
-summary(T2_aov)
-
-# You must submit your illustrations in an electronic form. Please submit the 
-# original graphic files. Do not insert figures into your Word document. 
-# Springer prefers that files be submitted in TIFF or EPS format, with at 
-# least 300 dpi resolution. PDF, JPG, Excel, and PowerPoint files may also 
-# be acceptable for simple figures but not for anything complex.  
-
-#All art must be submitted as electronic files. If necessary, please scan art at 600 dpi.
-
-#Remember when considering figures that they will probably be reduced to fit onto a book page, so pay particular attention to making labels highly legible and not too small. You may want to photocopy your figures to the expected final size as a test to determine legibility. The usable page size of the book will be about 4.5 x 7.5 inches. Most figures (including their caption) will occupy half a page. Labels should be in a sans-serif font, preferably Helvetica. When at final reduction the size should be no smaller than 8 point. Please ensure that your figures are clearly labeled.
-
-#For further information on figure formatting, please visit http://tinyurl.com/yzwf49l.
-
-#When preparing your figures, size figures to fit in the column width.
-#For most journals the figures should be 39 mm, 84 mm, 129 mm, or 174 mm wide and not higher than 234 mm.
-#For books and book-sized journals, the figures should be 80 mm or 122 mm wide and not higher than 198 mm.
-# mm to inches 0.0393701
-
-
-
-
-#
-# Figure 2
-#
-
-fw2 <- 0.0393701*90
-fh2 <- 0.0393701*80
-
-pdf("figure2.pdf",width = fw2, height = fh2)
-boxplot(T2$v_score ~ as.integer(T2$t_treatmenttype),ylab="Score",names=c("GOS","GOSup","JH"))
-dev.off()
-
-#
-# Figure 1
+# Figure 2 Observed stimuli figure
 #
 
 fw1 <- 0.0393701*122
@@ -92,7 +10,8 @@ fh1 <- 0.0393701*130
 
 dat <- readMat("Figure1.mat")
 
-pdf("Figure1.pdf",width = fw1, height = fh1)
+#pdf("Figure1.pdf",width = fw1, height = fh1)
+png(filename="Figure1.png",width = fw1, height = fh1, units="in",res=700)
 
 par(mfrow=c(3,3),omi=c(0.1,0.1,0.1,0.1),mar=c(4, 4.5, .8, .4), bty ="l")
 
@@ -117,10 +36,56 @@ mtext("(h)",side=3,line=0,adj=0)
 plot(dat$fig33[1,],dat$fig33[2,],ylim=c(40,140),type="l",xlab="frequency (Hz)",ylab="")
 mtext("(i)",side=3,line=0,adj=0)
 
-
-
 dev.off()
 
+#
+# Figure 3 Behaviour
+#
 
+#Manual scoring
+file <- 'C:/repositories/CollPen_mercurial/score_anova_simple.csv'
+dat<-read.table(file,sep = ";",header = TRUE)
+names(dat)
 
+# Checking data
+table(dat$b_block)
+table(dat$b_groupsize)
+table(dat$s_subblock)
+table(dat$s_treatmenttype)
+table(dat$t_treatment)
+table(dat$v_score)
+
+# Vessel noise data only
+T2<-dat[(dat$s_treatmenttype=='vessel'),]
+
+T2_aov <- aov(T2$v_score ~ T2$t_treatmenttype + T2$b_groupsize)
+summary(T2_aov)
+
+# VA/DP analysis
+library(XLConnect)
+va <- loadWorkbook("VAvessel.xls", create = F)
+dat<-readWorksheet(va,sheet="Sheet1", startRow = 0, endRow = 0, startCol = 0, endCol = 0)
+
+# VA ratios
+VA <- log(dat$sv/dat$sv_0)
+VA_aov <- aov(VA ~ dat$type + dat$groupsize)
+summary(VA_aov)
+
+DP <- (dat$m_0 - dat$m)
+DP_aov <- aov(DP ~ dat$type + dat$groupsize)
+summary(DP_aov)
+
+# Figure 3 plotting
+fw2 <- 0.0393701*90*2
+fh2 <- 0.0393701*80*2
+pdf("figure3.pdf",width = fw2, height = fh2)
+par(mfrow=c(3,1),omi=c(0.1,0.1,0.1,0.1),mar=c(4, 4.5, .8, .4), bty ="l")
+#boxplot(T2$v_score ~ as.integer(T2$t_treatmenttype) + T2$b_groupsize,ylab="Score",names=c("GOS","GOSup","JH"))
+boxplot(T2$v_score ~ as.integer(T2$t_treatmenttype) + T2$b_groupsize,ylab="Score ( )",names=F)
+mtext("(a)",side=3,line=0,adj=0)
+boxplot(DP ~ dat$type + dat$groupsize,ylab="Depth change (m)",names=F)
+mtext("(b)",side=3,line=0,adj=0)
+boxplot(VA ~ dat$type + dat$groupsize,ylab="log[VA] ( )")
+mtext("(c)",side=3,line=0,adj=0)
+dev.off()
 
