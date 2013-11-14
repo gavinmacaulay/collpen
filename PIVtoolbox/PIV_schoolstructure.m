69f9f43815b0e964a6ad4df8c180be15f59f4deb
nilsolav <nilsolav@OBSE6803-4.imr.no>
2013-11-14 08:51:00 +0100

    PIV schoolstructure updated

--- PIVtoolbox/PIV_schoolstructure.m
+++ PIVtoolbox/PIV_schoolstructure.m
@@ -1,63 +1,83 @@
-function [image, filepathbg] = PIV_schoolstructure(folder, avifilename, par)
+function [avspeed,dalpha,dcav,mcav2] = PIV_schoolstructure(xs,ys,us,vs,w, par)
 %
-% Estimate school structure parameters and average speed 
-% 
+% Estimate school structure parameters and average speed
+%
 % Input:
-% file          : The PIV mat file (Avi file name from didson without the .avi extension)
-% filedir       : Directory to store output files
+% xs, ys - positions
+% us, vs - velocities
+% w      - weigths
+% par.templag - The number of frames to calculate the temporal correlation
+% structure
 %
-% parstr        : Parameter structure
-% 
-% The inuput file should contain:
+% Output:
+% avspeed : w-weighted average speed in the movie
+% dalpha  : w-weighted temporal correlations
+% cav     : w-weighted angular velocity
 
-load(fullfile(folder,[avifilename,'_RAWPIV.mat']))
 
-% Remoce NaNs
-ind=isnan(snrs);
+% Remove NaNs
+ind=isnan(us)|isnan(vs);
 
 % Include fish/no fish filter
-%TODO
-fish = true(size(snrs));
-fish(ind)=false;
 
-% Calculate weights
-msnrs=par.msnrs;
-ssnrs=par.ssnrs;
-w =.5*(1+erf((snrs-msnrs)/sqrt(2*ssnrs^2)));
-
-% Quality weights
-qlt = w.*fish;
-
-% Calculate speed
+% Calculate speed matrix
 speed = hypot(us(:,:,:),vs(:,:,:));
+
 % Calculate angle matrix
 angle = atan2(us(:,:,:),vs(:,:,:));
 
-% Average speed weighted by quality and fish mask
-avspeed = sum(speed(~ind).*qlt(~ind)) / sum(qlt(~ind));
+% Average speed weighted by w
+avspeed = sum(speed(~ind).*w(~ind)) / sum(w(~ind));
 
-% temporal correlation structure
+% temporal correlation structure vector
 lag = 1:par.templag;
 
-uangle= unwrap(angle(:,:,:),[],3);
-dalpha = zeros(size(uangle,1),size(uangle,2),par.templag);
+% Temporal correlation structure array
+dalpha = zeros(size(angle,1),size(angle,2),par.templag);
 
 for i=lag
-    temp = nan(size(uangle,1),size(uangle,2),(size(uangle,3)-i-1));
-    for j=1:(size(uangle,3)-i-1)
-        temp(:,:,j) = abs(uangle(:,:,j)-uangle(:,:,j+i));
+    temp = nan(size(angle,1),size(angle,2),(size(angle,3)-i-1));
+    w2 = nan(size(angle,1),size(angle,2),(size(angle,3)-i-1));
+    % Loop over angle difference at given time lag (i) at each spatial cell
+    for j=1:(size(angle,3)-i-1)
+        temp(:,:,j) = abs(angle(:,:,j)-angle(:,:,j+i));
+        % Combine the weights for each time step
+        w2(:,:,j) = w(:,:,j) .* w(:,:,j+1);
     end
-    % Unweigthed mean, needs to be refined
-    dalpha(:,:,i)=mean(temp,3);
+    % Change from [0 2*pi> to [0 pi>
+    temp(temp>pi)=abs(temp(temp>pi) -2*pi);
+    % Change from NaN to 0 in the weights and data
+    temp(isnan(temp))=0;
+    w2(isnan(w2))=0;
+    % Take the average across time steps (dim 3)
+    dalpha(:,:,i)=sum(temp.*w2,3)./sum(w2,3);
 end
 
-meanalpha = nan([1 length(lag)]);
-for i=lag
-    dum =dalpha(:,:,i).*qlt(:,:,;
-    meanalpha(i) = mean(dum(:));
+% Spatial curvature (curl)
+dcav = zeros(size(angle,1),size(angle,2),size(angle,3));
+mcav = zeros([size(angle,3) 1]);
+
+% Loop over time steps
+for i=1:size(us,3)
+    % Combine the weights for each time step
+    w2(:,:,j) = w(:,:,j) .* w(:,:,j+1);
+    
+    % Compute the curl (a) and the angular velocity (b) in radians per unit
+    % time (which is half the |curl U|)
+    [~,cav]=curl(xs(:,:,i), ys(:,:,i),us(:,:,i),vs(:,:,i));
+
+    % Since we are using a center differencin scheme, the weights needs to
+    % be recalculated across 3x3 pixels
+    w2 = filter2([0 1 0;1 2 1;0 1 0]/6,w(:,:,i));
+    w2(isnan(w2)|isnan(cav))=0;
+    cav(isnan(cav))=0;
+
+    dcav(:,:,i) = cav;
+    % Take the average across each frame
+    dum=abs(dcav(:,:,i)).*w2;
+    mcav(i)=sum(dum(:))./sum(w2(:));
 end
 
-% Spatial correlation structure
-
-
+% The average across frames
+mcav2= mean(mcav);
 
--- PIVtoolbox/PIV_scr_example.m
+++ PIVtoolbox/PIV_scr_example.m
@@ -1,36 +1,31 @@
+clear
+close all
+
 % Example script for the CollPen PIV analysis toolbox
 %
 % (c) Lars Helge Stien/Nils Olav Handegard
 
-% Define the 4 Different examples
-
-% Dense milling
-file{1} = 'predmodel2013_White net_didson_block39_sub1';
-filedir{1} = 'data';
-par(1).showmsg=20;
-par(1).etc = 1;%etc
-
-% Sparse
-file{2} = 'predmodel2013_Orca_didson_block72_sub1';
-filedir{2} = 'data';
-par(2).N=20;
-par(2).etc = 1;%etc
 
 % Dense dynamic (bottle treatment)
 file{3} = 'didson_block37_sub1_treat1';
-filedir{3} = 'data';
-par(3).N=20;
-par(3).etc = 1;%etc
+for i=1:9
+    filedir{i} = 'data';
+end
 
-% Sparse dynamic (bottle treatment)
-file{4} = 'nn';
-filedir{4} = 'data';
-par(4).N=20;
-par(4).etc = 1;%etc
-   
-% Process the stationary case, look for swimming speed, school structure
+file{1} = 'predmodel2013_White net_didson_block39_sub1';
+file{2} = 'predmodel2013_White net_didson_block44_sub1';
+file{3} = 'predmodel2013_White net_didson_block54_sub1';
+file{4} = 'predmodel2013_White net_didson_block53_sub1';
+file{5} = 'predmodel2013_Brown net_didson_block52_sub1';
+file{6} = 'predmodel2013_Brown net_didson_block50_sub1';
+file{7} = 'predmodel2013_Brown net_didson_block46_sub1';
+file{8} = 'predmodel2013_Brown net_didson_block45_sub1';
+file{9} = 'predmodel2013_White net_didson_block43_sub1';
+
+
+%% Process the stationary case, look for swimming speed, school structure
 % (temporal and spatial)
-for i=2:2
+for i=1:9
     disp(['Running example on file: ' num2str(i) ', ' file{i} '.']);
     
     % Establish background image
@@ -39,24 +34,34 @@ for i=2:2
     disp(['BGImage saved as: ' filepathbg]);
         
     % Estimate flow fields
-    parstrpiv128= struct('showmsg',1,'winsize',128,'olap',0.75,'write',1,'useold',1);
+    %parstrpiv128= struct('showmsg',1,'winsize',128,'olap',0.75,'write',1,'useold',1);
     parstrpiv64 = struct('showmsg',1,'winsize',64,'olap',0.75,'write',1,'useold',1);
-    parstrpiv32 = struct('showmsg',1,'winsize',32,'olap',0.75,'write',1,'useold',1);
-    parstrpiv16 = struct('showmsg',1,'winsize',16,'olap',0.75,'write',1,'useold',1);  
-    [datapath rawpivel] = PIV_getRawPIVvectors(filedir{i}, file{i}, parstrpiv128);
-        
-    % Filter flow field - Her kan det eksperimenteres 
+    %parstrpiv32 = struct('showmsg',1,'winsize',32,'olap',0.75,'write',1,'useold',1);
+    %parstrpiv16 = struct('showmsg',1,'winsize',16,'olap',0.75,'write',1,'useold',1);  
+    
+    [datapath rawpivel] = PIV_getRawPIVvectors(filedir{i}, file{i}, parstrpiv64);
+    
+    % Filter flow field - Her kan det eksperimenteres
     parstrfilt = struct('showmsg',1,'global',4,'timeaverage',5,'localmedian',[3 3],'backgroundimage',bgimage);
     [xs ys us vs snrs pkhs is] = PIV_filterPIVvectors(rawpivel.xs, rawpivel.ys, rawpivel.us, rawpivel.vs, rawpivel.snrs, rawpivel.pkhs, rawpivel.is, parstrfilt);
     
-    % Illustrate the PIVs
-    dparstravi = struct('showmsg',1,'id','19');
-    avipath = PIV_createAVIfigure(filedir{i}, file{i}, xs, ys, us, vs, snrs, pkhs, is, dparstravi);
+    % Weigths
+    par(i).w = struct('msnrs',1.3,'ssnrs',0.5,'mthr',10,'sthr',7);
+    w=PIV_weights(snrs,pkhs,is,par(i).w);
+    
+    % Calculate school metrics
+    par(i).templag=20;
+    [speed{i},dalpha{i},dcav{i},cav{i}]=PIV_schoolstructure(xs,ys,us,vs,w,par(i));
+    
+    save didsonresults speed dalpha dcav cav
+    
+    % Create AVI
+    dparstravi = struct('showmsg',1,'id','');
+    avipath = PIV_createAVIfigure(filedir{i}, file{i}, xs, ys, us, vs, w, pkhs, is, dparstravi);
     disp(['Avi showing results stored in: ' avipath]);
-
 end
 
-% Probably different for the dynamic example
+%% Probably different for the dynamic example
 for i=3:4
 
     % Establish background image
