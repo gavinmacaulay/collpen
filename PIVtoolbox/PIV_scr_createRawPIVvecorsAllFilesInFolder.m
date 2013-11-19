d72ce699221ab003d1989406dd9d6dd6031c2512
Lars <Lars@localhost>
2013-11-19 08:50:28 +0100

    Commenting out the 32 and 128.

--- PIVtoolbox/PIV_scr_createRawPIVvecorsAllFilesInFolder.m
+++ PIVtoolbox/PIV_scr_createRawPIVvecorsAllFilesInFolder.m
@@ -19,21 +19,21 @@ for i=1:numoffiles
     disp(['..BGImage saved as: ' filepathbg]);
     
     disp(['..File: ' filepath]);
-    disp('....Setting: 128');
-    [datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv128);
+    %disp('....Setting: 128');
+    %[datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv128);
     disp('....Setting: 64');
     [datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name,parstrpiv64);
-    disp('....Setting: 32');
-    [datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv32);
-    disp('....Setting: 16');
-    [datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv16);
+    %disp('....Setting: 32');
+    %[datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv32);
+    %disp('....Setting: 16');
+    %[datapath rawpivel] = PIV_getRawPIVvectors(folder, files(i).name, parstrpiv16);
     t=toc;
    
-   totaltime=t+totaltime;
-   timeleft = totaltime/double(i)*numoffiles-totaltime;
+   totaltime = t+totaltime;
+   timeleft  = totaltime/double(i)*numoffiles-totaltime;
    
    disp(['..Calculation time: ' num2str(t/60) ' min']);
-   disp(['Totaltime so far: ' num2str(totaltime) ' min']);
-   disp(['Estimated time left: ' num2str(timeleft) ' min']);
+   disp(['Totaltime so far: ' num2str(totaltime/60) ' min']);
+   disp(['Estimated time left: ' num2str(timeleft/60) ' min']);
     
 end
