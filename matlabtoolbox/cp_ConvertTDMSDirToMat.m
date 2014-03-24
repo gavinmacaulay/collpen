6eb806bfa35654f89f4b43fc115f1876e0cf112f
gavinj <gavinj@localhost>
2014-03-24 11:46:22 +0100

    Tidy up formatting.

--- matlabtoolbox/cp_ConvertTDMSDirToMat.m
+++ matlabtoolbox/cp_ConvertTDMSDirToMat.m
@@ -5,19 +5,19 @@ function cp_ConvertTDMSDirToMat(dataDir,channelsToExport)
 % Designed for the NTNU hydrophone data from the Austevoll Collpen
 % experiments.
 %
-% channels is converting only the cannels specified
+% channels is converting only the channels specified
 
 d = dir(fullfile(dataDir, '*.tdms'));
 %warning('Hack')
 for i = 1:length(d)
 %for i = 12:length(d)
     disp(['File ',num2str(i),' of ',num2str(length(d))])
-    if exist(fullfile(dataDir,d(i).name))
-        if nargin<2
+    if exist(fullfile(dataDir,d(i).name), 'file')
+        if nargin < 2
             try
                 cp_ReadAndSaveTDMSFile(dataDir, d(i).name);
             catch
-                    warning([d(i).name,' failed'])
+                warning([d(i).name,' failed'])
             end
         else
             cp_ReadAndSaveTDMSFile(dataDir, d(i).name,channelsToExport);
