f7062c2cfc4d51ba1de4fc0d8c53d8d09dbe5898
gavinj <gavinj@localhost>
2014-03-24 11:47:22 +0100

    Initial version of code to process and display the NTNU hydrophone array data

---
+++ cpscr_processarraydata.m
@@ -0,0 +1,28 @@
+%% Process hydrophone array data and create figures in the figure directory
+
+% Data directory
+par.datadir = '\\callisto\collpen\AustevollExp\data\HERRINGexp';
+par.reposdir = '.\matlabtoolbox';
+
+% Parameters and metadata
+file = fullfile(par.datadir,'CollPenAustevollLog.xls');
+block = cp_GetExpPar(file);
+
+% Parameters for the spectrogram
+par.avgtime = 0.1;%s
+par.p_ref = 1e-6; % [Pa]
+par.ws=10000*60*4;%4 min of 8khz sampling
+par.Nwindow=256*4;
+par.Nfft=256*8;
+par.Noverlap=fix(par.Nwindow/2);
+
+% Example data for vessel avoidance and killer whale play back (for
+% plotting a nice conceptual figure for the papers)
+par.exampleVA = [21,1];
+par.exampleKW = [21,4];
+par.CPA = 38;
+
+%% Run
+for i=21 %21:35
+    cp_ProcessArraydata(block(i).b_block,block,par);
+end
---
+++ matlabtoolbox/cp_ProcessArraydata.m
@@ -0,0 +1,110 @@
+function cp_ProcessArraydata(blockn,block,par)
+
+% This is the block, subblock, treatment vector. If the vecotr is shorter,
+%
+% Parameters for picking the right data
+
+% block(blockn).subblock(subblockn).treatment(treatmentn)
+%
+% %%%Note that this was changed  on 2 july to 118.816 Pa/V for block 10 and greater
+% par.nexus1.cal=115.4 % Pa/volt
+%
+% % This is our calibration after changing the NExus
+% %par.nexus2.cal= 118.816% 37.6% Pa/volt Note that this was changed  on 2 july to 118.816 Pa/V for block 10 and greater
+%
+% par.nexus2.cal= 37.6% 37.6% Pa/volt FOR BLOCK 9 AND LOWER , 118.816 for block 10 and higher
+
+% Load array data, but only those files that start with the block number
+files = dir(fullfile(par.datadir,['block',num2str(blockn)],'hydrophones',[num2str(blockn) '_*.mat']));
+
+if isempty(files)
+    error(['No Matlab format hydrophone files available for block ' num2str(blockn) '. Run preparation script.'])
+end
+
+% There are two hydrophone arrays, each containing 8 hydrophones.
+% Array 1 has 8 hydrophones connected to channels 1 to 8 and array 2 has
+% hydrophones connected to channels 9 to 16. The hydrophones were spaced
+% every 1 m and the topmost was 2 m deep. Channels 17 and 18 recorded data
+% from the two IMR hydrophones.
+
+array = struct('channel', 1:16, ...
+    'depth', [9 8 7 6 5 4 3 2 9 8 7 6 5 4 3 2], ... % [m]
+    'array', [1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2]);
+
+for f=1:length(files)
+    
+    file =  fullfile(par.datadir,['block',num2str(blockn)],'hydrophones',files(f).name);
+    hdr = ['array_',files(f).name(1:end-4)];
+    
+    % What kind of subblock/treatment is this?
+    [a,~] = sscanf(files(f).name(1:end-4),'%u_%u_%u');
+    subblock  = a(2);
+    treatment = a(3);
+    
+    % only interested in vessel treatments for now.
+    if strcmp(block(a(1)).subblock(subblock).s_treatmenttype,'vessel')
+        disp(['Processing file: ' files(f).name ...
+            ' (treatment is ' block(a(1)).subblock(a(2)).treatment(a(3)).t_treatmenttype ')'])
+        
+        clear press press_rms_avg time ind_start ind_end
+        
+        load(file)
+        press = zeros(16, size(data.values,1));
+        
+        for chan = 1:16
+            par.Fs = data.sample_rate(chan);
+            par.start_time = data.start_time(chan);
+            par.avg_bin = floor(par.Fs*par.avgtime);  % average into 1/10 second  bins
+            press(chan,:) = data.values(:,chan).*block(blockn).b_nexus1sens;  % pressure in Pa
+
+            % compute RMS pressure by averaging
+            for i = 1:floor(length(press(chan,:))/par.avg_bin)-1
+                ind_start(i)=((i-1)*(par.avg_bin))+1;
+                ind_end(i)=((i)*(par.avg_bin));
+                % compute rms via a homebrew function by taking rms of small section
+                temp = press(chan,ind_start(i):ind_end(i)) - mean(press(chan,ind_start(i):ind_end(i))); %  % these are de-trended as per Nils Olav's suggestion
+                press_rms_avg(chan, i)= (mean(temp.^2))^.5;
+                time(i) = ((mean([ind_start(i) ind_end(i)])))./(par.Fs);
+            end
+        end        
+        clear data
+        
+        % Plot spectrogram
+        par.figdir = fullfile(par.datadir,'figures', 'array');
+        if ~exist(par.figdir, 'dir')
+            mkdir(par.figdir)
+            warning('figure directory not created - fixed')
+        end
+
+        % basic spectrogram of a representative channel
+        exChan = 1;
+        figure(1)
+        spectrogram(double(press(exChan,:)).*1e6,par.Nwindow,par.Noverlap,par.Nfft,par.Fs,'yaxis');
+        title([hdr,'_psd_chan' num2str(exChan)], 'Interpreter', 'none')
+        colorbar
+        xlabel('Time (s)')
+        print('-dpng','-r200',fullfile(par.figdir,[hdr,'_psd_chan' num2str(exChan) '.png']))
+        
+        % plot of pressure as a function of time
+        figure(2)
+        plot((1:length(press(exChan,:)))./(par.Fs), press(exChan,:))
+        xlabel('Time (s)')
+        ylabel('Pressure (Pa)')
+        title([hdr,'_chan' num2str(exChan) '_pressure'],'Interpreter','none')
+        print('-dpng','-r200',fullfile(par.figdir,[hdr,'_chan' num2str(exChan) '_pressure.png']))
+        
+        % plot SPL
+        figure(3)
+        plot(time, 20*log10(press_rms_avg(exChan,:)./par.p_ref))
+
+        xlabel('Time (s)')
+        ylabel('SPL dB re 1 \mu Pa')
+        title([hdr,'_SPL'],'Interpreter','none')
+        print('-dpng','-r200',fullfile(par.figdir,[hdr,'_SPL.png']))
+        
+        % plot of SPL for all hydrophones in array, arranged to show
+        % variation with depth
+        
+        
+    end
+end
