b2588d0c4a264cbea751427232c855997edba2de
gavinj <gavinj@localhost>
2014-03-26 09:45:47 +0100

    Added comments about which block we are using

--- cpscr_processarraydata.m
+++ cpscr_processarraydata.m
@@ -22,8 +22,19 @@ par.exampleVA = [21,1];
 par.exampleKW = [21,4];
 par.CPA = 38;
 
+par.calibration = [ones(1,16)*11.53 1 1]; % [Pa/V]
+
 %% Run
-for i = 21:35
+
+% some blocks had problems with the recording of array data (channels
+% missing and others full of noise). Channel 1 never seemed to work and
+% channel 8 was intermittent. Blocks 27, 28, and 29 are the best set of
+% data with only channel 1 missing. Block 27_3_2 has some noise at the end
+% of the recording, and block 29 is noisy, so we focus
+% on block 28 for the manuscript...
+
+par.export_plot = true;
+for i = 28 %21:35 
     cp_ProcessArraydata(block(i).b_block,block,par);
 end
 
