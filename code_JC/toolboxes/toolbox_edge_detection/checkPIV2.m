bbe0a2da4480317e94bffef4e153b24dc19cdad2
jccmontoya <jccmontoya@localhost>
2015-07-31 13:31:15 +0200

    This function checks the performance of the PIV algorithm against a set of manually labelled points that corresponds to fish positions.

--- code_JC/toolboxes/toolbox_edge_detection/checkPIV2.m
+++ code_JC/toolboxes/toolbox_edge_detection/checkPIV2.m
@@ -1,12 +1,46 @@
 function [angles ranges dot_products relative_range] = ...
-            checkPIV2(file, pr_frames, pr_x, pr_y, movieobj, px_meter, ...
-                      fps, save_path, save_image)
+    checkPIV2(file, pr_frames, pr_x, pr_y, px_meter, ...
+              fps, save_path, save_image, movieobj)
+% 
+% This function checks the performance of the PIV algorithm against a set 
+% of manually labelled points that corresponds to fish positions.
+%
+% Inputs:
+%   - file: The input PIVs are given by 'file' variable, wich refers to a .m file
+%           with the following content:
+%        · frames        : Frames to index the positions   
+%        · frames_interpolated: Not used
+%        · predator_x    : x dimension of the labelled position           
+%        · interp_x      : Not used             
+%        · predator_y    : x dimension of the labelled position      
+%        · interp_y       : Not used       
+%   - pr_frames: Frames to index the groundtruth positions to the PIVs
+%   - pr_x: x position of the labelled position
+%   - pr_y: y position of the labelled position 
+%   - px_meter: deprecated
+%   - fps: deprecated
+%   - save_image: flag to indicate whether save the comparison as images
+%   - save_path: path to save comparison images if save_image is active
+%   - movieobj: descriptor of the video object pointing to the source video
+%               from which PIVs have been calculated
+%
+% Outputs:
+%   - angles: Angle difference between PIVs and groundtruth
+%   - ranges: Lenght difference between PIVs and groundtruth
+%   - dot_product: Dotproduct between PIVs and groundtrut
+%   - relative_range: Normalized length difference between PIVs and groundtruth
+  
+disp('[checkPIV2]: Start');
 
-close
-load(file);
+angles          = [];
+ranges          = [];
+dot_products    = [];
+relative_range  = [];
+preys           = [];
+
+load(file); % PIVs file
 
 % Cluster manually labeled prey positions
-preys = [];
 for i = 3:3:length(pr_frames)
     fr = pr_frames(i-2:i);
     px = pr_x(i-2:i);
@@ -15,15 +49,11 @@ for i = 3:3:length(pr_frames)
     preys = [preys ; [fr(2) x y u v]];
 end
 
+% The size of the image from which PIVs are obtained is necessary
 I = read(movieobj, 1);
-[r c n] = size(I);
 if(n>1)
     I = rgb2gray(I);
 end
-angles = [];
-ranges = [];
-dot_products = [];
-relative_range = [];
 
 if(save_image)
     f = figure('Units','normalized','Position',[0 0 0.5 1]);
@@ -35,7 +65,6 @@ for i = 1:length(preys)
     if(save_image)
         clf(f,'reset');
         I = read(movieobj, preys(i,1));
-        [r c n] = size(I);
         if(n>1)
             I = rgb2gray(I);
         end
@@ -49,25 +78,12 @@ for i = 1:length(preys)
     piv_u = US(round(preys(i,3)),round(preys(i,2))); % In pixels/sec
     piv_v = VS(round(preys(i,3)),round(preys(i,2))); % In pixels/sec
     
-%     piv_u = piv_u/fps;
-%     piv_v = piv_v/fps;
-    
-%     piv_u = piv_u / px_meter;
-%     piv_v = piv_v / px_meter;
-    
-    %    subsampled_x = round(preys(i,2)*100/c);
-    %    subsampled_y = round(preys(i,3)*100/r);
-    
     vvv = vs(:,:,preys(i,1)); % In pixels/sec
     uuu = us(:,:,preys(i,1)); % In pixels/sec
-    %vvv = vvv / px_meter; % In meters/sec No conversion, just for display
-    %uuu = uuu / px_meter; % In meters/sec
     xxx = xs(:,:,preys(i,1));
     yyy = ys(:,:,preys(i,1));
     prey_u = preys(i,4); % In pixels/frame
     prey_v = preys(i,5); % In pixels/frame
-%     prey_u = prey_u * fps / px_meter; % In meters/sec
-%     prey_v = prey_v * fps / px_meter; % In meters/sec
     prey_x = preys(i,2);
     prey_y = preys(i,3);
     
@@ -78,37 +94,23 @@ for i = 1:length(preys)
         
         plot(pr_x((i*3)-2:1:i*3),pr_y((i*3)-2:1:i*3),'.g');
         F = getframe(f);
-        % saveas(f, img_save_path,'epsc2');
-        saveas(f, img_save_path);
+        saveas(F, img_save_path);
         
-        %imwrite(F.cdata,img_save_path);
     end
+           
+    [prey_r prey_len] = cartesian2Polar(prey_u,prey_v);
+    [piv_r piv_len] = cartesian2Polar(piv_u,piv_v);
     
-    
-    %  piv_u = uuu(subsampled_x,subsampled_y);
-    %  piv_v = vvv(subsampled_x, subsampled_y) ;
-    %   uuu(subsampled_y,subsampled_x)
-    %   vvv(subsampled_y,subsampled_x)
-    
-    [prey_theta prey_len] = cartesian2Polar(prey_u,prey_v);
-    [piv_theta piv_len] = cartesian2Polar(piv_u,piv_v);
-    
-    %     prey_theta = mod((prey_theta+180),360) - 180
-    %     piv_theta = mod((piv_theta+180),360) - 180
-    %
-    %     angle = piv_theta - prey_theta;
-    len = piv_len - prey_len;
-    
-    relative_range = [relative_range ; piv_len*100/prey_len];
-    
-    angle = getAngleTwoVectors([prey_u,prey_v],[piv_u,piv_v]);
-    dot_product = dot([prey_u,prey_v],[piv_u,piv_v]);
-    
-    angles = [angles ; angle];
-    ranges = [ranges ; len];
-    dot_products = [dot_products ; dot_product];
+    len             = piv_len - prey_len;
+    relative_range  = [relative_range ; piv_len*100/prey_len];
+    angle           = getAngleTwoVectors([prey_u,prey_v],[piv_u,piv_v]);
+    dot_product     = dot([prey_u,prey_v],[piv_u,piv_v]);
+    angles          = [angles ; angle];
+    ranges          = [ranges ; len];
+    dot_products    = [dot_products ; dot_product];
     
 end
+disp('[checkPIV2]: End');
 
 end
 
