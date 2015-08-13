7161ab1e648f78b87e8f0883a4f2d73f7a7a11b4
jccmontoya <jccmontoya@localhost>
2015-08-13 10:04:02 +0200

    Added subscript to generate videos for the methods paper

--- code_JC/script_render_avi_from_polar_mat.m
+++ code_JC/script_render_avi_from_polar_mat.m
@@ -29,7 +29,7 @@ for i = 1:length(ddf_files)
     try
         load(ddf_files(i).name);
         avi_out = strrep(ddf_files(i).name, '.mat', '.avi');
-        mat_to_avi(D,avi_out,frame_rate);
+        mat_to_avi(D,avi_out,frame_rate,-1, -1);
         
     catch
         disp('Wrong mat data file');
@@ -39,5 +39,68 @@ for i = 1:length(ddf_files)
 end
 disp('End');
 
+%% Render avi files from single videos
 
+frame_rate = 8;
+
+ddf_path    = '/Volumes/Datos/collpen/denoising_tests/sources/';
+
+ddf_file    = [ddf_path '2013-07-17_085620.mat'];
+avi_out   = [ddf_path '2013-07-17_085620_1825_1920_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,1825,1920);
+
+ddf_file    = [ddf_path '2013-07-17_171402.mat'];
+avi_out   = [ddf_path '2013-07-17_171402_1844_1947_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,1844,1947);
+
+ddf_file    = [ddf_path '2013-07-18_090802.mat'];
+avi_out   = [ddf_path '2013-07-18_090802_1735_1810_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,1735,1810);
+
+ddf_file    = [ddf_path '2013-07-18_120502.mat'];
+avi_out   = [ddf_path '2013-07-18_120502_1450_1520_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,1450,1520);
+
+ddf_file    = [ddf_path '2013-07-18_161457.mat'];
+avi_out   = [ddf_path '2013-07-18_161457_726_804_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,726,804);
+
+ddf_file    = [ddf_path '2013-07-19_105504.mat'];
+avi_out   = [ddf_path '2013-07-19_105504_1758_1821_raw_polar.avi'];
+load(ddf_file);
+mat_to_avi(D,avi_out,frame_rate,1758,1821);
+
+
+%%
+
+ddf_path    = '/Volumes/Datos/collpen/denoising_tests/sources/';
+
+ddf_file    = [ddf_path '2013-07-17_085620.avi'];
+avi_out   = [ddf_path '2013-07-17_085620_1825_1920_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,1825,1920);
+
+ddf_file    = [ddf_path '2013-07-17_171402.avi'];
+avi_out   = [ddf_path '2013-07-17_171402_1844_1947_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,1844,1947);
+
+ddf_file    = [ddf_path '2013-07-18_090802.avi'];
+avi_out   = [ddf_path '2013-07-18_090802_1735_1810_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,1735,1810);
+
+ddf_file    = [ddf_path '2013-07-18_120502.avi'];
+avi_out   = [ddf_path '2013-07-18_120502_1450_1520_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,1450,1520);
+
+ddf_file    = [ddf_path '2013-07-18_161457.avi'];
+avi_out   = [ddf_path '2013-07-18_161457_726_804_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,726,804);
+
+ddf_file    = [ddf_path '2013-07-19_105504.avi'];
+avi_out   = [ddf_path '2013-07-19_105504_1758_1821_raw_cartesian.avi'];
+change_video_framerate_resolution(ddf_file,avi_out,0,0,0,1758,1821);
 
