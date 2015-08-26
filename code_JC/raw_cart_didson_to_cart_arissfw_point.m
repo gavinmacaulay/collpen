7ffeae1a9a268951e75001f1597c239263a79fe4
jccmontoya <jccmontoya@localhost>
2015-08-26 10:15:47 +0200

    Function to convert points between raw cartesian do ARIS software generated videos (for DIDSON input information)

---
+++ code_JC/raw_cart_didson_to_cart_arissfw_point.m
@@ -0,0 +1,13 @@
+function pout = raw_cart_didson_to_cart_arissfw_point(pin)
+
+pin_x_corrected = double(pin(1) - 3);
+
+pin_x_corrected = int32(pin_x_corrected * 524 / 394);
+pin_x_corrected = pin_x_corrected + 81;
+
+pin_y_corrected = double(pin(2));
+
+pin_y_corrected = int32(pin_y_corrected * 1022 / 744);
+pin_y_corrected = pin_y_corrected + 22;
+pout = [pin_x_corrected pin_y_corrected];
+end
